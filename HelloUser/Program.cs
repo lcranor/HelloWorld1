09a649ea678d72affdc5b987ee430c8a11c2a25f
Lesley Cranor <lesleycranor@gmail.com>
2017-09-24 15:22:36 -0500

    first commit

---
+++ HelloUser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloUser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello User!");
+        }
+    }
+}
